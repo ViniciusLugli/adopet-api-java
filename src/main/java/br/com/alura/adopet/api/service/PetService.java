d276bf9e158be8f5a479671b734e4b3d27f51a99
Vinicius <viniciusflugli@gmail.com>
2026-01-06 19:57:28 -0300

    feat: melhorias aplicadas no Pet

---
+++ src/main/java/br/com/alura/adopet/api/service/PetService.java
@@ -0,0 +1,20 @@
+package br.com.alura.adopet.api.service;
+
+import br.com.alura.adopet.api.dto.DadosDetalhesPet;
+import br.com.alura.adopet.api.model.Pet;
+import br.com.alura.adopet.api.repository.PetRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class PetService {
+    @Autowired
+    private PetRepository repository;
+
+    public List<DadosDetalhesPet> listarPets() {
+        List<Pet> pets = repository.findByAdotadoIsFalse();
+        return pets.stream().map(DadosDetalhesPet::new).toList();
+    }
+}
